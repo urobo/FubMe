26cf5101005e9ca37181baccb4f012cf9194076e
riccardo buttarelli <rico.sleeps@gmail.com>
2011-05-18 18:02:02 +0200

    TimelineManager

--- src/org/fubme/persistency/TimelineManager.java
+++ src/org/fubme/persistency/TimelineManager.java
@@ -3,9 +3,16 @@
  */
 package org.fubme.persistency;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import org.fubme.factories.PostFactory;
 import org.fubme.models.Post;
 import org.fubme.models.User;
 
@@ -14,8 +21,37 @@ import org.fubme.models.User;
  * 
  */
 public abstract class TimelineManager {
-	static final List<Post> getTimelineForUser(User user) {
+	static final List<Post> getTimelineForUser(User user, int limit) {
 		List<Post> result = new ArrayList<Post>();
+		Connection connection = DBConnection.getConnection();
+		Statement stmt = null;
+		String sql = 	"SELECT * from post where luser_id in (select luser_id_followed from luser_follows_luser where luser_id_follower = '"+user.getId()+"') and luser_id  not in (select wrongdoing_id from luser_reports_luser where whistleblower_id = '"+user.getId()+"') limit "+ limit;
+		try {
+			stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_UPDATABLE);
+			ResultSet timeline = stmt.executeQuery(sql);
+			while(timeline.next()){
+				Post post = PostFactory.getPost(timeline.getTimestamp(Post.PTIME),
+						timeline.getInt(Post.ID), 
+						timeline.getString(Post.LUSER_ID),
+						timeline.getString(Post.BODY),
+						timeline.getString(Post.LINK),
+						timeline.getString(Post.MIME));
+				result.add(post);
+			}
+		} catch (SQLException ex) {
+			Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+		} finally {
+			if (stmt != null)
+				stmt = null;
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+
+				}
+				connection = null;
+			}
 		return result;
 	}
 }
---
+++ src/org/fubme/test/TimelineManagerTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.fubme.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author riccardo
+ *
+ */
+public class TimelineManagerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.fubme.persistency.TimelineManager#getTimelineForUser(org.fubme.models.User, int)}.
+	 */
+	@Test
+	public void testGetTimelineForUser() {
+		fail("Not yet implemented");
+	}
+
+}
